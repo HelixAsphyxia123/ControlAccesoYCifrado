f1e8ebf13b3020d49a3a2cafe8bfa015a863241c
HelixAsphyxia123 <71672918+HelixAsphyxia123@users.noreply.github.com>
2020-11-02 14:47:51 -0600

    Update Modelo.java

--- pruebalogin/Modelo.java
+++ pruebalogin/Modelo.java
@@ -11,29 +11,6 @@ import java.security.NoSuchAlgorithmException;
 
 public class Modelo {
 
-    Vista v = new Vista();
-
-    public String convertirSHA256(String password) {
-
-        MessageDigest md = null;
-        try {
-
-            md = MessageDigest.getInstance("SHA-256");
-
-        } catch (NoSuchAlgorithmException e) {
-            System.out.print("error" + e.getMessage());
-            return null;
-        }
-
-        byte[] hash = md.digest(password.getBytes());
-        StringBuffer sb = new StringBuffer();
-
-        for (byte b : hash) {
-            sb.append(String.format("%02x", b));
-
-        }
-
-        return sb.toString();
-    }
+   //Se creo una libreria.
 
 }
